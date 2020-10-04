70c7b09f15363f611c7ed951a5a7208b0ffabbec
samart007 <samart1460@gmail.com>
2020-10-04 17:11:35 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const BlogIndex = ({ data, location }) => {
         <SEO title="All posts" />
         <Bio />
         <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
+          samart  สร้าง No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
           gatsby-config.js).
         </p>
